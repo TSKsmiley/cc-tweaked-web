6484e82dcf855f36e023be306c4b1a0fd229ba07
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2021-12-30 17:48:27 +0100

    example data

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var path = require('path');
+var itemsExample = require('./itemsExample.json');
 
 var app = express();
 
 app.set('view engine', 'pug')
-
 app.set('views', path.join(__dirname, '/Views'));
 
 app.use(express.json());
 
-app.get('/', function(req, res){
-    res.send('Hello World');
 
+app.get('/', function(req, res){
+    res.render('index', {items: itemsExample});
 });
     
 
